0508a257a65e2aafa45b178603a7fbc44ac2cb56
justaworker <1438367681@qq.com>
2019-02-21 15:22:26 +0800

    3

--- src/views/blog-module/blogBook.vue
+++ src/views/blog-module/blogBook.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="page-container">
-    <div class="main-container">
-      <div class="blogs-con">
+  <div class="page-container" :style="bgStyle" ref="pageContainer">
+    <div class="bg-mask"></div>
+    <nav class="page-nav">
+      <i class="el-icon-menu"></i>
+      <span class="nav-menu" @click="$router.push('/chat')">chat</span>
+      <span class="nav-menu" @click="$router.push('/home')">home</span>
+    </nav>
+    <div class="main-container" ref="blogMain" @scroll="bgScroll()">
+      <div class="blogs-con" id="blogs-con">
         <el-row>
           <el-card>
             <div class="card-header">
@@ -12,10 +18,9 @@
               </div>
             </div>
             <div class="card-content-con">
-              <p class="card-text">
-                Keep close to Nature's heart... and break clear away, once in awhile,
-                and climb a mountain or spend a week in the woods. Wash your spirit clean.
-              </p>
+              <p
+                class="card-text"
+              >Keep close to Nature's heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods. Wash your spirit clean.</p>
               <div class="card-image-con">
                 <div class="card-image">
                   <img src="@/assets/images/login_bg.png">
@@ -35,10 +40,117 @@
               </div>
             </div>
             <div class="card-content-con">
-              <p class="card-text">
-                This is content, without any paragraph or header tags,
-                within an ion-card-content element.
-              </p>
+              <p
+                class="card-text"
+              >This is content, without any paragraph or header tags, within an ion-card-content element.</p>
+              <div class="card-image-con">
+                <div class="card-image">
+                  <img src="@/assets/images/login_bg.png">
+                </div>
+                <div class="card-image">
+                  <img src="@/assets/images/home_bg.jpeg">
+                </div>
+                <!-- <img class="card-image" src="@/assets/images/login_bg.png">
+                <img class="card-image" src="@/assets/images/home_bg.jpeg">-->
+              </div>
+            </div>
+          </el-card>
+          <div class="blog-footer"></div>
+        </el-row>
+        <el-row>
+          <el-card>
+            <div class="card-header">
+              <img class="card-avatar" src="@/assets/images/logo.jpeg">
+              <div class="card-title-con">
+                <div class="card-subtitle">Card Subtitle</div>
+                <div class="card-title">Card Title</div>
+              </div>
+            </div>
+            <div class="card-content-con">
+              <p
+                class="card-text"
+              >This is content, without any paragraph or header tags, within an ion-card-content element.</p>
+              <div class="card-image-con">
+                <div class="card-image">
+                  <img src="@/assets/images/login_bg.png">
+                </div>
+                <div class="card-image">
+                  <img src="@/assets/images/home_bg.jpeg">
+                </div>
+                <!-- <img class="card-image" src="@/assets/images/login_bg.png">
+                <img class="card-image" src="@/assets/images/home_bg.jpeg">-->
+              </div>
+            </div>
+          </el-card>
+          <div class="blog-footer"></div>
+        </el-row>
+        <el-row>
+          <el-card>
+            <div class="card-header">
+              <img class="card-avatar" src="@/assets/images/logo.jpeg">
+              <div class="card-title-con">
+                <div class="card-subtitle">Card Subtitle</div>
+                <div class="card-title">Card Title</div>
+              </div>
+            </div>
+            <div class="card-content-con">
+              <p
+                class="card-text"
+              >This is content, without any paragraph or header tags, within an ion-card-content element.</p>
+              <div class="card-image-con">
+                <div class="card-image">
+                  <img src="@/assets/images/login_bg.png">
+                </div>
+                <div class="card-image">
+                  <img src="@/assets/images/home_bg.jpeg">
+                </div>
+                <!-- <img class="card-image" src="@/assets/images/login_bg.png">
+                <img class="card-image" src="@/assets/images/home_bg.jpeg">-->
+              </div>
+            </div>
+          </el-card>
+          <div class="blog-footer"></div>
+        </el-row>
+        <el-row>
+          <el-card>
+            <div class="card-header">
+              <img class="card-avatar" src="@/assets/images/logo.jpeg">
+              <div class="card-title-con">
+                <div class="card-subtitle">Card Subtitle</div>
+                <div class="card-title">Card Title</div>
+              </div>
+            </div>
+            <div class="card-content-con">
+              <p
+                class="card-text"
+              >This is content, without any paragraph or header tags, within an ion-card-content element.</p>
+              <div class="card-image-con">
+                <div class="card-image">
+                  <img src="@/assets/images/login_bg.png">
+                </div>
+                <div class="card-image">
+                  <img src="@/assets/images/home_bg.jpeg">
+                </div>
+                <!-- <img class="card-image" src="@/assets/images/login_bg.png">
+                <img class="card-image" src="@/assets/images/home_bg.jpeg">-->
+              </div>
+            </div>
+          </el-card>
+          <div class="blog-footer"></div>
+        </el-row>
+        <el-row>
+          <el-card>
+            <div class="card-header">
+              <img class="card-avatar" src="@/assets/images/logo.jpeg">
+              <div class="card-title-con">
+                <div class="card-subtitle">Card Subtitle</div>
+                <div class="card-title">Card Title</div>
+              </div>
+            </div>
+            <div class="card-content-con">
+              <p
+                class="card-text"
+              >This is content, without any paragraph or header tags, within an ion-card-content element.</p>
               <div class="card-image-con">
                 <div class="card-image">
                   <img src="@/assets/images/login_bg.png">
@@ -60,22 +172,105 @@
 
 <script>
 import { mapGetters } from "vuex";
+import html2canvas from "html2canvas";
 export default {
   data() {
-    return {};
+    return {
+      bgStyle: {}
+    };
   },
   computed: {
     ...mapGetters(["user"])
   },
-  mounted() {},
-  methods: {}
+  mounted() {
+    this.getBgImg();
+  },
+  methods: {
+    getBgImg() {
+      html2canvas(document.getElementById("blogs-con"), {
+        logging: false
+      }).then(
+        canvas => {
+          const url = canvas.toDataURL("image/png");
+          this.$refs.pageContainer.style.backgroundImage = "url(" + url + ")";
+          // this.bgStyle = {
+          //   "background-image": "url(" + url + ")"
+          // };
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    },
+    bgScroll() {
+      const scrollTop = this.$refs.blogMain.scrollTop;
+      const positionTop = -scrollTop * (100 / (100 - 10 - 8 - 10));
+      // const imageUrl = this.bgStyle["background-image"];
+      this.bgStyle = {
+        // "background-image": imageUrl,
+        "background-position-y": positionTop + "px"
+      };
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+$images: "../../assets/images/";
+.page-container {
+  height: 100vh;
+  padding: 10vh 20vh;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  background-repeat: no-repeat;
+  background-size: 1000px;
+  background-position-x: center;
+  overflow-x: hidden;
+  overflow-y: auto;
+  .bg-mask {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0px;
+    left: 0px;
+    background-color: #eef2f7f5;
+    background-image: url($images+"bg001.jpg");
+    opacity: 0.95;
+  }
+  .page-nav {
+    width: 100%;
+    margin: auto;
+    min-height: 60px;
+    display: flex;
+    align-items: center;
+    background-color: #fafbfcf2;
+    max-width: 700px;
+    z-index: 2;
+    .el-icon-menu {
+      font-size: 20px;
+      margin-left: 16px;
+    }
+    .nav-menu {
+      margin: 16px;
+      font-size: 20px;
+      font-weight: 500;
+    }
+  }
+  .main-container {
+    max-width: 700px;
+    margin: auto;
+    z-index: 2;
+    overflow-y: auto;
+  }
+}
+
 .main-container {
+  // height: 100%;
+
   .blogs-con {
-    max-width: 640px;
+    // padding: 50px;
+    // max-width: 640px;
     .card-header {
       display: flex;
       align-items: center;
@@ -93,7 +288,6 @@ export default {
         color: #8a9aa9;
       }
     }
-
     .card-text {
       font-size: 15px;
       line-height: 1.6;
--- src/views/blog-module/index.vue
+++ src/views/blog-module/index.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="blog-module" class="module-container">
-    <nav class="nav-container">
-      <ul class="nav-ul">
-        <li @click="goRoute(navs['home'].path)">{{navs['home'].name}}</li>
-        <li @click="goRoute(navs['chat'].path)">{{navs['chat'].name}}</li>
-        <!-- <li @click="goRoute(navs['game'].path)">{{navs['game'].name}}</li> -->
-      </ul>
-    </nav>
+    <!-- <nav class="nav-container">
+        <ul class="nav-ul">
+          <li @click="goRoute(navs['home'].path)">{{navs['home'].name}}</li>
+          <li @click="goRoute(navs['chat'].path)">{{navs['chat'].name}}</li>
+        </ul>
+    </nav>-->
     <router-view/>
   </div>
 </template>
@@ -20,4 +19,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#blog-module {
+    height: 100vh;
+    overflow: hidden;
+  .blog-nav {
+    // position: absolute;
+    // top: 40px;
+    // background-color: #fafbfc;
+    // margin: 0px 60px;
+  }
+}
 </style>
